127a38501aff4a3ac77296927a9d30b5aa4f5814
ErikBoesen <me@erikboesen.com>
2016-07-26 23:11:45 -0400

    Incredibly sketchy save path modification system, bug fixes (#1 #2 #3 (maybe) #5 #6)

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ const fs = require('fs');
 
 // Define prominent buttons.
 var submit = document.getElementById('submit'),
-	reset = document.getElementById('reset');
+	reset = document.getElementById('reset'),
+    path = document.getElementById('path'),
+    pathWarning = document.getElementById('path-warning');
+
+function pathInit() {
+    path.value = (process.platform == 'win32') ? process.env.USERPROFILE + '\\Desktop\\data.json' : process.env.HOME + '/Desktop/data.json';
+}
+
+pathInit();
 
 // Generate an array of all <input>s (plus <select>s) in document.
 // These will be used to generate an object.
-var tags = document.querySelectorAll('input, select');
+// Inputs named .special are exempt. These are used for things like path selection.
+var tags = document.querySelectorAll('input:not(.special), select');
 // Create empty object.
 var inputs = {};
 // Make each element be the value to a key named after its ID.
@@ -25,20 +34,33 @@ submit.onclick = function() {
 		data[input] = (inputs[input].type === 'number') ? parseInt(inputs[input].value) : inputs[input].value;
 	}
 
-    // Reset <input>s to prepare for new contents after submission
-    resetInputs();
-
 	// Add timestamp to data.
 	data['timestamp'] = new Date().getTime();
 
 	// Log gathered data to console, useful for debug
 	// console.log(data);
 
-	// Append new JSON-parsed data to data.json file on desktop.
-	// (For Windows, to get the user home dir, you need to get process.env.USERPROFILE, for everything else process.env.HOME.)
-	fs.appendFile(process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'] + '/Desktop/data.json', JSON.stringify(data) + '\n');
+	// Append new JSON-parsed data to data.json file in designated location (usually Desktop).
+	fs.appendFile(path.value, JSON.stringify(data) + '\n', function(err) {
+        if (err) {
+            pathInit();
+            pathWarning.innerHTML = 'NOT A VALID PATH!!!!';
+            path.focus();
+            throw err;
+        } else {
+            pathWarning.innerHTML = '';
+            // Reset <input>s to prepare for new contents after submission
+            resetInputs();
+        }
+    });
 };
 
+path.onblur = function() {
+    pathWarning.innerHTML = 'Trying to submit w/ new path...';
+    submit.click();
+};
+
+// When reset button is clicked, trigger reset
 reset.onclick = resetInputs();
 
 // Reset all fields without submitting any data.
@@ -61,7 +83,7 @@ function resetInputs() {
 // When user clicks on the screen, check if they clicked on an increase/decrease button
 onclick = function(e) {
 	// If click was on a decrease button > increase the value of the adjacent input
-	if (e.target.className === 'decrease') e.target.nextElementSibling.value = parseInt(e.target.nextElementSibling.value) - 1;
+	if (e.target.className === 'decrease' && e.target.nextElementSibling.value > 0) e.target.nextElementSibling.value = parseInt(e.target.nextElementSibling.value) - 1;
 	// If click was on an increase button > decrease the value of the adjacent input
 	if (e.target.className === 'increase') e.target.previousElementSibling.value = parseInt(e.target.previousElementSibling.value) + 1;
 };
