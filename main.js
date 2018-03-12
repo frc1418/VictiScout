bfbdeaadc842e433e2fc70be803f189eb380779f
ErikBoesen <me@erikboesen.com>
2018-03-12 18:05:16 -0400

    Remove extraneous commenting in main.js

--- main.js
+++ main.js
@@ -26,27 +26,22 @@ function createWindow() {
 
 	// Emitted when the window is closed.
 	mainWindow.on('closed', function() {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
+		// Dereference window object
 		mainWindow = null;
 	});
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Called when Electron has finished initialization.
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
+	// On OS X, stay active until the user quits explicitly
 	if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', function() {
-	// On OS X it's common to re-create a window in the app when the
+	// On OS X, re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (mainWindow === null) createWindow();
 });
@@ -62,9 +57,7 @@ ipcMain.on('renderData', function(event, arg) {
 	dataWindow.loadURL(`file://${__dirname}/data.html`);
 
 	dataWindow.on('closed', function() {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
+		// Dereference window object
 		dataWindow = null;
 	});
 });
